88d2ca6a1849534c6e45b69cf2d0c2a82057e6c7
0zl <zwiefk0@gmail.com>
2023-09-25 03:52:14 +0700

    weird aiohttp

--- modules/rds/async_rds_host_only.py
+++ modules/rds/async_rds_host_only.py
@@ -28,15 +28,102 @@ class RDSClient:
         self.global_chan = cmd_opts.rds_global
         self.ts_started = int(round(time.time() * 1000))
     
+    async def send_data(self, data, success=True, request_id=None, channel=None):
+        if hasattr(data, '__dict__'):
+            data = data.__dict__
+        elif type(data) is list:
+            list_data = []
+            for i, e in enumerate(data):
+                list_data.append(e)
+            data = { 'list': list_data }
+        elif isinstance(data, str):
+            data = { 'info': data }
+
+        n_data = { 'success': success, **data }
+        await self.client.publish(
+            self.mother if channel is None else channel,
+            serialize(self.identifier, n_data, callbackId=request_id)
+        )
+        
+    async def ping_status(self, channel=None):
+        print('w4 - gc ping')
+        
+        url = f'http://127.0.0.1:{cmd_opts.port}'
+        async with aiohttp.ClientSession() as session:
+            async with session.head(url) as resp:
+                if resp.status == 200:
+                    status = 'online'
+                else:
+                    if shared.is_model_ready:
+                        status = 'offline'
+                    else:
+                        status = 'booting'
+                        
+        await self.send_data({
+            'id': self.identifier,
+            'ts': self.ts_started,
+            'status': status,
+            'busy_queue': shared.state.job_count
+        }, channel=channel)
+    
+    async def request_api(self, task, params, method, chan_name, request_id):
+        url = f'http://127.0.0.1:{cmd_opts.port}/sdapi/v1{task}'
+        
+        try:
+            async with aiohttp.ClientSession() as session:
+                if method == 'GET':
+                    async with session.get(url, params=params) as resp:
+                        r = resp
+                elif method == 'POST':
+                    async with session.post(url, json=params) as resp:
+                        r = resp
+                else:
+                    async with session.get(url, params=params) as resp:
+                        r = resp
+                
+                if r.status_code == 200:
+                    data = await r.json()
+                    await self.send_data(data, True, request_id, chan_name)
+                else:
+                    data = await r.text()
+                    await self.send_data(data, False, request_id, chan_name)
+        except Exception as e:
+            await self.send_data(str(e), False, request_id, chan_name)
+    
+    async def handle_global_call(self, msg: PubSub):
+        if msg['channel'] != self.global_chan:
+            return False
+        
+        await self.ping_status()
+        return True
+
+    async def handle_tasks(self, msg: PubSub):
+        data = json.loads(msg['data'])
+        
+        chan_name = data['from']
+        request_id = data['requestId']
+        
+        try:
+            task_name = data['data']['task']
+            task_params = data['data']['params']
+            task_method = data['data']['method']
+        except Exception as e:
+            await self.send_data(str(e), False, request_id, chan_name)
+            return
+        
+        if task_name.startswith('/'):
+            await self.request_api(task_name, task_params, task_method, chan_name, request_id)
+        else:
+            await self.ping_status(chan_name)
+    
     async def process_message(self, msg: PubSub):
         if msg['type'] not in ['subscribe', 'message']:
             return
         
-        if not isinstance(msg['data'], str):
-            print(msg)
+        if await self.handle_global_call(msg):
             return
         
-        print(msg)
+        await self.handle_tasks(msg)
     
     async def launch_async(self):
         self.client = aioredis.Redis(
