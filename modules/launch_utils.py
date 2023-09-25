3e82fd37508a58582412008502237eda18a6b198
0zl <69357965+0zl@users.noreply.github.com>
2023-09-26 05:44:22 +0700

    yes yes, ik

--- modules/launch_utils.py
+++ modules/launch_utils.py
@@ -428,7 +428,7 @@ def configure_for_tests():
 
 
 def start():
-    print(f"Launching {'API server' if '--nowebui' in sys.argv else 'Web UI'} with arguments: {' '.join(sys.argv[1:])}")
+    print(f"Arguments: {' '.join(sys.argv[1:])}")
     import webui
     
     if '--rd' in sys.argv:
